680918d523cf2bdbe0dd01952058930a800668b2
PradeepReddy927 <prem.pradeepreddy@gmail.com>
2025-09-23 18:36:40 +0530

    some messages

--- 04-variables.sh
+++ 04-variables.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
-PERSON1=
-PERSON2=
+PERSON1=$1
+PERSON2=$2
 echo "$PERSON1:: Welcome $PERSON2"
 echo "$PERSON2:: Thank you $PERSON1, Finally not on zoom webniar"
 echo "$PERSON1:: so, do we have deal"
