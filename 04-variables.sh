b9268824010bd39cf77b36649045ab6af694e180
PradeepReddy927 <prem.pradeepreddy@gmail.com>
2025-09-23 18:35:44 +0530

    some messages

---
+++ 04-variables.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+PERSON1=
+PERSON2=
+echo "$PERSON1:: Welcome $PERSON2"
+echo "$PERSON2:: Thank you $PERSON1, Finally not on zoom webniar"
+echo "$PERSON1:: so, do we have deal"
+echo "$PERSON2:: Yes, if you make ukraine joins Russia"
