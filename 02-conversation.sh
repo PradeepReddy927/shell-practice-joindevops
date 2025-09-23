dc3e774ed861ec10bbd8c36580f126993fcde54c
PradeepReddy927 <prem.pradeepreddy@gmail.com>
2025-09-23 11:58:26 +0530

    new conversation

---
+++ 02-conversation.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+
+echo "Ramesh:: Hi suresh, how are you?"
+echo "suresh:: hello Ramesh Iam fine how are you?"
+echo "Ramesh:: what are you learning recently?"
+echo "suresh:: I am learning shell scripting Ramesh"
