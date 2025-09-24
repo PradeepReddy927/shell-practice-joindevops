9ce1ea3cdbf53c3b44ef5f1143eb3998a0b0c6b4
PradeepReddy927 <prem.pradeepreddy@gmail.com>
2025-09-24 10:25:23 +0530

    some messages

--- 08-special-vars.sh
+++ 08-special-vars.sh
@@ -4,4 +4,6 @@
  echo "All variables passed to the script: $*"
 
  echo "Script name:$0"
- echo "Current directory :$PWD"
+ echo "Current directory: $PWD"
+ echo "Who us running this: $USER"
+ echo "User home directory: $HOME" 
