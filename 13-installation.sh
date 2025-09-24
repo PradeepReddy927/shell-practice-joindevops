a8cbd8c430bc78e73b27609a22f4d2fb0620d98d
PradeepReddy927 <prem.pradeepreddy@gmail.com>
2025-09-24 18:16:33 +0530

    some messages

---
+++ 13-installation.sh
@@ -0,0 +1,30 @@
+#!/bin/bash
+
+USERID=$(id -u)
+
+if [ $USERID -ne 0 ]
+then
+echo "ERROR: Please run this script with root privileges"
+exit 1 # failure is other than 0
+fi
+
+VALIDATE(){ # functions receive inputs through args just like shell script args
+ if [ $1 -ne 0 ]
+ then
+ echo "ERROR: Installing $2 is failure"
+ exit 1
+ else
+ echo "Installing $2 is Success"
+ fi
+
+}
+
+dnf install mysql -y
+VALIDATE $? "MYSQL"
+
+dnf install nginx -y
+VALIDATE $? "nginx"
+
+dnf install mongodb-mongosh -y
+VALIDATE $? "mongosh"
+
