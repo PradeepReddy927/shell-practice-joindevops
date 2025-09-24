8d4e944a37fbe236dd71c22e305e569d9917c4da
PradeepReddy927 <prem.pradeepreddy@gmail.com>
2025-09-24 16:02:25 +0530

    some messages

--- 10-conditions.sh
+++ 10-conditions.sh
@@ -2,11 +2,11 @@
 
 NUMBER=$1
 
-if [ $NUMBER -lt 10 ]
-then
-echo "Given number $NUMBER is less then 10"
+if [ $NUMBER -lt 10 ]; then
+
+    echo "Given number $NUMBER is less then 10"
 else
-echo "Given number $NUMBER is greater than or equal to 10"
+    echo "Given number $NUMBER is greater than or equal to 10"
 fi
 
 # -gt
