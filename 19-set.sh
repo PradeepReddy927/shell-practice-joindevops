0b72397d4c92cf6c7108cbc418490a6fef700018
PradeepReddy927 <prem.pradeepreddy@gmail.com>
2025-09-28 17:54:54 +0530

    some messages

--- 19-set.sh
+++ 19-set.sh
@@ -2,7 +2,13 @@
 
 set -e
 
+error(){
+    echo "There is a error" 
+}
+
+trap error ERR
+
 echo "Hello .."
 echo "Before error"
-uhdijjfijf
+uhdijjfijf;dnf # here shell understands there is an error and signal is ERR
 echo "After error"
