1a68e22bec973e75dac27b1ef5e8f12422d3ac92
PradeepReddy927 <prem.pradeepreddy@gmail.com>
2025-09-28 18:05:49 +0530

    some messages

--- 19-set.sh
+++ 19-set.sh
@@ -6,7 +6,7 @@ error(){
     echo "There is a error in $LINENO, Command is: $BASH_COMMAND"
 }
 
-trap error ERR
+trap 'echo "There is a error in $LINENO, Command is: $BASH_COMMAND"' ERR
 
 echo "Hello .."
 echo "Before error"
