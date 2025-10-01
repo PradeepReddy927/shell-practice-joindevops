0a46db9068aa6956dd8b52501f452e111fe0e818
PradeepReddy927 <prem.pradeepreddy@gmail.com>
2025-10-01 14:50:23 +0530

    some messages

--- 03-variables.sh
+++ 03-variables.sh
@@ -4,7 +4,7 @@
 PERSON1=John
 PERSON2=Abhi
 
-echo "$PERSON1:: Hi $PERSON1, how are you?"
+echo "$PERSON1:: Hi $PERSON2, how are you?"
 echo "$PERSON2:: hello $PERSON1, Iam fine how are you?"
 echo "$PERSON1:: what are you learning recently?"
 echo "$PERSON2:: I am learning shell scripting $PERSON1"
