42ea6b10b3e4334ff2dc961661981766e28ffc3c
PradeepReddy927 <prem.pradeepreddy@gmail.com>
2025-10-03 15:40:27 +0530

    some messages

--- 15-colors.sh
+++ 15-colors.sh
@@ -34,7 +34,7 @@ echo -e "MYSQL already Exist ... $Y SKIPPING $N"
 fi
 
 
-dnf list instaleed nginx 
+dnf list installed nginx
 if [ $? -ne 0 ]
 then
 dnf install nginx -y 
@@ -47,46 +47,6 @@ dnf list installed python3 &>>$LOGS_FILE
 if [ $? -ne 0 ]
 then
 dnf install python3 -y &>>$LOGS_FILE
-VALIDATE $? "python3"
-else
-echo -e "python3 already Exist ... $Y SKIPPING $N"
-fi
-
-VALIDATE(){ # functions receive inputs through args just like shell script args
- if [ $1 -ne 0 ]
- then
- echo -e "Installing $2 ... $R is failure $N"
- exit 1
- else
- echo -e "Installing $2 ... $G is Success $N"
- fi
-
-}
-
-dnf list installed mysql
-#Install if it is not found
-if [ $? -ne 0]
-then
-dnf install mysql -y
-VALIDATE $? "MYSQL"
-else
-echo -e "MYSQL already Exist ... $Y SKIPPING $N"
-fi
-
-
-dnf list instaleed nginx
-if [ $? -ne 0 ]
-then
-dnf install nginx -y
-VALIDATE $? "nginx"
-else
-echo -e "nginx already Exist ... $Y SKIPPING $N"
-fi
-
-dnf list installed python3
-if [ $? -ne 0 ]
-then
-dnf install python3 -y
 VALIDATE $? "python3"
 else
 echo -e "python3 already Exist ... $Y SKIPPING $N"
